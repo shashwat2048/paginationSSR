d4e4d9cbf198f8cc5e4b61edbe55b58b5566a888
shashwat2048 <shinghalshashwat@gmail.com>
2025-07-09 17:41:20 +0530

    fix: homepage props

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import { getImages } from "./utils/api";
 import Image from "next/image";
 import Pagination from './components/Pagination';
 import { PageProps } from "../../.next/types/app/layout";
-type HomeProps = { page?: string } & PageProps;
+type HomeProps = PageProps &  { searchParams : {page?: string} };
 type imgObj =  {
   "id": string,
   "author": string,
@@ -13,7 +13,7 @@ type imgObj =  {
   "download_url": string,
 }
 
-export default async function Home({ searchParams } : { searchParams : HomeProps}) {
+export default async function Home({ searchParams } : HomeProps) {
   const pageNum = parseInt(searchParams?.page ?? "1");
   const images = await getImages(pageNum);
 
